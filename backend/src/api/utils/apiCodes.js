25370860c0545d5b0c87ad7c842d8ddc253f5e6e
Rogério Araújo <rogerio.araujo@mail.com>
2020-06-03 11:52:41 -0300

    fix: correção na nomeclatura da pasta utils

--- backend/src/api/utils/apiCodes.js
+++ backend/src/api/utils/apiCodes.js
@@ -1,8 +1,9 @@
 /**
- * Define API code messages
+ * Define API code for reponse messages
  */
 module.exports = {
   S_API_STATUS: { code: 'S_API_STATUS', status: 200, message: 'API running successfully' },
   E_PERSON_NOT_FOUND: { code: 'E_PERSON_NOT_FOUND', status: 404, message: 'Person does not exist' },
   E_DUPLICATE_EMAIL: { code: 'E_DUPLICATE_EMAIL', status: 409, message: 'Validation error' },
+  E_NOT_FOUND: { code: 'E_NOT_FOUND', status: 404, message: 'Resource not found' },
 };
