4c7177cb74f601f24e8220c6dbf32128037e8004
Rogério Araújo <rogerio.araujo@mail.com>
2020-06-05 22:28:31 -0300

    api: atualizando url da api

--- backend/src/api/routes/v1/index.js
+++ backend/src/api/routes/v1/index.js
@@ -9,6 +9,17 @@ const router = express.Router();
 /**
  * GET v1/status
  */
+router.get('/', (req, res) =>
+  res.status(c.S_API_STATUS.status).send({
+    code: c.S_API_STATUS.code,
+    status: c.S_API_STATUS.status,
+    message: c.S_API_STATUS.message,
+  })
+);
+
+/**
+ * GET v1/status
+ */
 router.get('/status', (req, res) =>
   res.status(c.S_API_STATUS.status).send({
     code: c.S_API_STATUS.code,
--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('../api/routes/v1');
 
+const router = express.Router();
+
 /**
  * Express instance.
  *
@@ -14,6 +16,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+
+/**
+ * GET v1/status
+ */
+router.get('/', (req, res) => res.status(200).send('hellow'));
 
 app.use('/v1', routes);
 
